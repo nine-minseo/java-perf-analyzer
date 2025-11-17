8b15bbe7dd8b1522eb86660b0ac4547b5f7201ec
구민서 <minseo12379@gmail.com>
2025-11-17 11:35:51 +0900

    feat: 문자열 덧셈 성능 분석 클래스 추가

---
+++ src/main/java/performance/analysis/StringConcatAnalyze.java
@@ -0,0 +1,20 @@
+package performance.analysis;
+
+import performance.AnalyzeResult;
+import performance.PerformanceAnalyze;
+
+public class StringConcatAnalyze implements PerformanceAnalyze {
+    private static final String ANALYZE_NAME = "String 덧셈";
+
+    @Override
+    public String getAnalyzeName() {
+        return ANALYZE_NAME;
+    }
+
+    @Override
+    public AnalyzeResult runAnalyze(long iterations) {
+        long durationNano = 0;
+
+        return new AnalyzeResult(ANALYZE_NAME, durationNano, iterations);
+    }
+}
