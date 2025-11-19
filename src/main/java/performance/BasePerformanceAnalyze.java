92f2819ebd991604a261925e7dad61c6abf364f5
구민서 <minseo12379@gmail.com>
2025-11-19 14:09:36 +0900

    feat: 성능 분석 템플릿 메서드 구현

---
+++ src/main/java/performance/BasePerformanceAnalyze.java
@@ -0,0 +1,26 @@
+package performance;
+
+public abstract class BasePerformanceAnalyze implements PerformanceAnalyze {
+    private static final int WARM_UP_ITERATIONS = 1_000;
+
+    @Override
+    public final AnalyzeResult runAnalyze(long iterations) {
+        warmUp();
+        long durationNano = measure(iterations);
+        return new AnalyzeResult(getAnalyzeName(), durationNano, iterations);
+    }
+
+    private void warmUp() {
+        execute(WARM_UP_ITERATIONS);
+    }
+
+    private long measure(long iterations) {
+        long startTime = System.nanoTime();
+        execute(iterations);
+        long endTime = System.nanoTime();
+
+        return endTime - startTime;
+    }
+
+    protected abstract void execute(long iterations);
+}
